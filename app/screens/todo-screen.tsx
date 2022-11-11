4ebb0fa8b5a75b101f3a1436ee655c7f3ed0dd5f
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-11-11 11:26:16 +0700

    feat(screen/todo): add task input

--- app/screens/todo-screen.tsx
+++ app/screens/todo-screen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {TaskInput} from '../components';
 
 const TodoScreen = () => {
+  const [taskName, setTaskName] = useState('');
+  const onAddTask = () => {
+    if (taskName === '') {
+      return;
+    }
+    console.log('Task: ', taskName);
+    setTaskName('');
+  };
   return (
     <View>
       <Text style={styles.title}>Todo List</Text>
+      <TaskInput
+        value={taskName}
+        onChangeText={text => setTaskName(text)}
+        onAdd={onAddTask}
+      />
     </View>
   );
 };
